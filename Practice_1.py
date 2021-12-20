c220cc6cdb6851445100949bf9871bba28e6d4c9
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-12-19 23:35:47 -0700

    did the numbers themselves, not the order.

--- Practice_1.py
+++ Practice_1.py
@@ -1,11 +1,15 @@
+nums = [3,2,4]
+target = 6
 
-class solution(object):
-    def two_sum(self, nums, target):
-        for n in range(len(nums)):
-            first = nums.pop(0)
-            for number in nums:
-                if (number + first) == target:
-                    return [number, first]
-                else:
-                    pass
-            
+# class solution(object):
+def two_sum(nums, target):
+    for n in range(len(nums)):
+        first = nums.pop(0)
+        for number in nums:
+            if (number + first) == target:
+                return [first, number]
+            else:
+                pass
+
+
+print(two_sum(nums, target))
