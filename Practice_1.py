677f386abbb50336cbb1d1bf22c40be136ca98f3
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-12-19 23:40:19 -0700

    completed!

--- Practice_1.py
+++ Practice_1.py
@@ -1,15 +1,16 @@
-nums = [3,2,4]
+nums = [3,3]
 target = 6
-
 # class solution(object):
 def two_sum(nums, target):
+    nn = 0
     for n in range(len(nums)):
+        nnn = nn +1
         first = nums.pop(0)
         for number in nums:
             if (number + first) == target:
-                return [first, number]
+                return [nn, nnn]
             else:
-                pass
-
-
+                nnn = nnn + 1
+        nn = 1 + nn
+        
 print(two_sum(nums, target))
