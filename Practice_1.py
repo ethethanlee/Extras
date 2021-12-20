1db89c9cd71a6d58050a2e81297d5fc0627abe5a
Ethan Lee <63252912+ethethanlee@users.noreply.github.com>
2021-12-19 23:32:07 -0700

    made it

--- Practice_1.py
+++ Practice_1.py
@@ -1,4 +1,11 @@
 
-
-def two_sum(self, nums, target):
-    ok
+class solution(object):
+    def two_sum(self, nums, target):
+        for n in range(len(nums)):
+            first = nums.pop(0)
+            for number in nums:
+                if (number + first) == target:
+                    return [number, first]
+                else:
+                    pass
+            
