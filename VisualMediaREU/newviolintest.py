ff8994eab0ed578ee2b477502ff5448471ceaf62
ethethanlee <ethethanlee@gmail.com>
2022-07-20 14:18:54 -0700

    test images

--- VisualMediaREU/newviolintest.py
+++ VisualMediaREU/newviolintest.py
@@ -3,11 +3,11 @@ import numpy as np
 # import keyboard
 
 
-cap = cv2.VideoCapture('moreviolinimage.mp4')
+cap = cv2.VideoCapture('20JulyTestImages.mp4')
 frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-vidObj = cv2.VideoCapture('moreviolinimage.mp4')
+vidObj = cv2.VideoCapture('20JulyTestImages.mp4')
 frame = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
 
 fc = 0
@@ -43,6 +43,8 @@ while success:
         folder = 'colLegno'
     if value=='r':
         folder = 'pizz'
+    if value=='t':
+        folder = 'test'
     # Save Frame by Frame into disk using imwrite method
     cv2.imwrite('rawData/'+ folder + '/newFrame'+str(i)+'.jpg', frame)
     print('frame done')
